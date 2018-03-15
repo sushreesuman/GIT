a87ea08aaa5f697cb0de54acc9fcea92ce6786e0
sushreesuman <37334689+sushreesuman@users.noreply.github.com>
2018-03-15 16:11:50 +0530

    Commit and Push

--- Practice Java1/src/Test/Abc.java
+++ Practice Java1/src/Test/Abc.java
@@ -2,9 +2,14 @@ package Test;
 
 interface Abc {
 	public abstract void show();
+
+	public abstract void call();
 }
 class AbcImpl implements Abc{
 	public void show() {
 		System.out.println("in show");
+	}
+	public void call() {
+		System.out.println("in call");
 	}
 }
--- Practice Java1/src/Test/Sush.java
+++ Practice Java1/src/Test/Sush.java
@@ -4,5 +4,7 @@ public class Sush{
 public static void main (String[] args) {
 	Abc obj = new AbcImpl();
 	obj.show();
+	Abc obj1 = new AbcImpl();
+	obj1.call();
 }
 }
